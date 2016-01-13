87950d2f39f4e9b87713e53cb52e66e427abcd43
13240943@qq.com <gitmima123>
2016-01-13 14:25:33 +0800

    init Idea project folder

---
+++ oschina/src/oschina/IT100/IT37.java
@@ -0,0 +1,13 @@
+package oschina.IT100;
+/**
+ * @project: oschina
+ * @filename: IT37.java
+ * @version: 0.10
+ * @author: JM Han
+ * @date: 2:25 PM 1/13/2016
+ * @comment: Test Purpose
+ * @result: Your test result
+ */
+
+public class IT37 {
+}
