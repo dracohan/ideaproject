fafbbecd4f609bc2c7e1daa0996068b6c237b5ea
dracohan <13240943@qq.com>
2016-01-17 14:55:55 +0800

    add IT39

---
+++ oschina/src/oschina/IT100/IT39.java
@@ -0,0 +1,13 @@
+package oschina.IT100;
+/**
+ * @project: oschina
+ * @filename: IT39.java
+ * @version: 0.10
+ * @author: JM Han
+ * @date: 14:55 2016/1/17
+ * @comment: Test Purpose
+ * @result:
+ */
+
+public class IT39 {
+}
