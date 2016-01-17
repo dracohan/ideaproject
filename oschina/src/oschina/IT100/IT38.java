c41bf701e31f749b3b4890839feb0c21374510ed
dracohan <13240943@qq.com>
2016-01-17 14:54:55 +0800

    add IT38

---
+++ oschina/src/oschina/IT100/IT38.java
@@ -0,0 +1,13 @@
+package oschina.IT100;
+/**
+ * @project: oschina
+ * @filename: IT38.java
+ * @version: 0.10
+ * @author: JM Han
+ * @date: 14:53 2016/1/17
+ * @comment: Test Purpose
+ * @result:
+ */
+
+public class IT38 {
+}
