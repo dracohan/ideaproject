70ba0a1c8504d932f1f42bc1af66e4828a89c609
dracohan <13240943@qq.com>
2016-01-17 23:07:19 +0800

    update @home 20160117

---
+++ LearningJava/src/com/bwhan/c04/InstanceOf.java
@@ -0,0 +1,36 @@
+package c04;
+/**
+ * @project: LearningJava
+ * @filename: InstanceOf.java
+ * @version: 0.10
+ * @author: JM Han
+ * @date: 15:53 2016/1/17
+ * @comment: Test Purpose
+ * @result:
+ */
+
+class Base{
+
+}
+
+class Derived extends Base {
+
+}
+
+
+public class InstanceOf {
+	public static void main(String[] args) {
+		Derived d = new Derived();
+		if (d instanceof Base)
+			System.out.println("Yes, derived object is also instance of Base");
+		else
+			System.out.println("No, derived object is not instance of Base");
+
+		Derived[] darr = new Derived[10];
+
+		if(darr instanceof Base[])
+			System.out.println("Yes, derived object array is also instance of Base array");
+		else
+			System.out.println("No, derived object array is not instance of Base array");
+	}
+}
