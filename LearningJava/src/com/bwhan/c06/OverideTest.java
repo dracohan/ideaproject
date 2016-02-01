115bd45489836b446d8686b05eca9e240ef7e224
dracohan <13240943@qq.com>
2016-02-01 20:48:28 +0800

    update LearnJava @home 20160201

--- LearningJava/src/com/bwhan/c06/OverideTest.java
+++ LearningJava/src/com/bwhan/c06/OverideTest.java
@@ -12,11 +12,12 @@ package c06;
 
 class B {
 	final int i = 1;
+	int k = 3;
 	final void prtb(){
 		System.out.println("basic class func read i: " + i);
 	}
 
-	static void func(){
+	void func(){
 		System.out.println("B.func()");
 	}
 }
@@ -28,7 +29,8 @@ class D extends B{
 		System.out.println("derived class func read i: " + i);
 	}
 
-    static void func(){
+
+    void func(){
 		System.out.println("D.func()");
 	}
 }
@@ -36,19 +38,31 @@ class D extends B{
 public class OverideTest {
 	public static void main(String[] args) {
 		B b = new B();
+		//基类对象访问基类成员
 		System.out.println("basic class i: "+b.i);
 
 		D d = new D();
+		//派生类访问派生类成员
 		System.out.println("derived class i:"+d.i);
 		d.prtb();
 		d.prtd();
 
+		//访问继承而来的成员变量
+		System.out.println("k from B: " + d.k);
+
 		B bb = new D();
+		//转换成了父类型了子类特有的变量就不再可以访问了
 		//System.out.println(bb.j);
+		//访问基类继承而来的类型也是父类性的值
 		System.out.println("read i of derived class with basic class handle: "+bb.i);
 
-		B.func();
-		D.func();
+		//实际的类型有关
+		b.func();
+		d.func();
+		bb.func();
+
+
+
 
 	}
 }
