4056f7e048e4073e5f0ee55cb368fca0a3745dd8
13240943@qq.com <gitmima123>
2016-02-04 12:21:29 +0800

    update code @office 20160204

---
+++ oschina/src/mianjing/tencent.java
@@ -0,0 +1,16 @@
+package mianjing;
+/**
+ * @project: oschina
+ * @filename: tencent.java
+ * @version: 0.10
+ * @author: JM Han
+ * @date: 12:17 PM 2/4/2016
+ * @comment: Test Purpose
+ * @result: Your test result
+ */
+
+public class tencent {
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+}
